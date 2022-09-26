5a6ce91e6b6c3c374177ba43ef075d69f6d4dc83
Deraas <Bendik.Deraas@gmail.com>
2022-09-26 18:05:04 +0200

    Biotitle

--- client/src/Bio.tsx
+++ client/src/Bio.tsx
@@ -13,12 +13,15 @@ function Bio() {
   return (
     <>
       <div className="bioContainer">
-        <div className="bioText">{data.map((bio: any) => bio.text)}</div>
         <img
           src={data.map((bio: any) => bio.imageUrl + "?w=1000&fit=scale")}
           alt="import"
           className="bioImage"
         />
+        <div className="bioTextContainer">
+          <h2 className="bioTitle">{data.map((bio: any) => bio.title)}</h2>
+          <div>{data.map((bio: any) => bio.text)}</div>
+        </div>
       </div>
     </>
   );
