77076541f863415e4ece5367cc4dd5e73818ee0c
Deraas <Bendik.Deraas@gmail.com>
2022-10-25 22:21:46 +0200

    Kolonner i Galleri

--- client/src/Verk.tsx
+++ client/src/Verk.tsx
@@ -2,29 +2,39 @@ import React from "react";
 import "./App.css";
 import { useVerk } from "./useVerk";
 
+function split(array: any, arraycount: number) {
+  let n = Math.round(array.length / arraycount);
+  let [...arr] = array;
+  var res = [];
+
+  while (arr.length) {
+    res.push(arr.splice(0, n));
+  }
+  return res;
+}
+
 function Verk() {
   const { data } = useVerk();
   //console.log(data);
   if (!data) {
     return null;
   }
+  let columns = split(data, 4);
 
+  console.log(columns);
   return (
     <div className="galleri">
-      {data.map((image: any) => (
-        <div className="verk" key={image.verkkode}>
-          <img
-            src={image.croppedImageUrl + "?w=300&fit=scale"}
-            alt="import"
-            // onMouseOver={(e) =>
-            //   ( e.currentTarget.src =
-            //     image.landscapeImageUrl + "?h=300&fit=scale")
-            // } //TODO: Must be dynamically cropped and fit same scale as cropped image url
-            // onMouseOut={(e) =>
-            //   (e.currentTarget.src = image.croppedImageUrl + "?h=300&fit=scale")
-            //}
-          ></img>
-          <p>{image.name}</p>
+      {columns.map((column: any) => (
+        <div className="column">
+          {column.map((image: any) => (
+            <div className="verk" key={image.verkkode}>
+              <img
+                src={image.croppedImageUrl + "?w=300&fit=scale"}
+                alt="import"
+              ></img>
+              <p>{image.name}</p>
+            </div>
+          ))}
         </div>
       ))}
     </div>
@@ -32,3 +42,11 @@ function Verk() {
 }
 
 export default Verk;
+
+// onMouseOver={(e) =>
+//   ( e.currentTarget.src =
+//     image.landscapeImageUrl + "?h=300&fit=scale")
+// } //TODO: Must be dynamically cropped and fit same scale as cropped image url
+// onMouseOut={(e) =>
+//   (e.currentTarget.src = image.croppedImageUrl + "?h=300&fit=scale")
+//}
