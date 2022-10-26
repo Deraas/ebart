ee52cc559a0ae1bae66ee6cd77477009fa11c9ee
Deraas <Bendik.Deraas@gmail.com>
2022-10-26 20:00:32 +0200

    Added mouseover(fail)

--- client/src/Verk.tsx
+++ client/src/Verk.tsx
@@ -15,13 +15,13 @@ function split(array: any, arraycount: number) {
 
 function Verk() {
   const { data } = useVerk();
+  const scalingOptions = "?w=800&fit=scale";
   //console.log(data);
   if (!data) {
     return null;
   }
   let columns = split(data, 4);
 
-  console.log(columns);
   return (
     <div className="galleri">
       {columns.map((column: any) => (
@@ -29,16 +29,16 @@ function Verk() {
           {column.map((image: any) => (
             <div className="verk" key={image.verkkode}>
               <img
-                src={image.croppedImageUrl + "?w=600&fit=scale"}
+                src={image.croppedImageUrl + scalingOptions}
                 alt="import"
-                // onMouseOver={(e) =>
-                //   (e.currentTarget.src =
-                //     image.landscapeImageUrl + "?h=300&fit=scale")
-                // } //TODO: Must be dynamically cropped and fit same scale as cropped image url
-                // onMouseOut={(e) =>
-                //   (e.currentTarget.src =
-                //     image.croppedImageUrl + "?h=300&fit=scale")
-                // }
+                onMouseOver={(e) =>
+                  (e.currentTarget.src = image.landscapeImageUrl
+                    ? image.landscapeImageUrl + scalingOptions
+                    : image.croppedImageUrl + scalingOptions)
+                } //TODO: Must be dynamically cropped and fit same scale as cropped image url
+                onMouseOut={(e) =>
+                  (e.currentTarget.src = image.croppedImageUrl + scalingOptions)
+                }
               ></img>
               <div className="verktittel">{image.name}</div>
               <div className="verktekst">
