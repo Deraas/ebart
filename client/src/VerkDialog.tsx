a734232c366308f0634b179f02fa1923981477f6
Deraas <Bendik.Deraas@gmail.com>
2022-11-15 21:44:33 +0100

    WIP dialog

--- client/src/VerkDialog.tsx
+++ client/src/VerkDialog.tsx
@@ -10,7 +10,7 @@ function VerkDialog(props: {
   image: any;
 }) {
   const { isOpen, setIsOpen } = props;
-  const scalingOptions = "?w=800&fit=scale";
+  const scalingOptions = "?w=1000&fit=scale";
   const image = props.image;
 
   return (
@@ -22,13 +22,19 @@ function VerkDialog(props: {
         {/* Full-screen container to center the panel */}
         <div className="verkcontainer">
           <Dialog.Panel className={"verkdialog"}>
-            <img
-              src={image.croppedImageUrl + scalingOptions}
-              alt="import"
-            ></img>
-            <Dialog.Title> </Dialog.Title>
-            <Dialog.Description>
-              This will permanently deactivate your account
+            <Dialog.Title className={"tittel"}>
+              {image.name} <span className="year">{image.year}</span>
+            </Dialog.Title>
+
+            <Dialog.Description className={"description"}>
+              <img
+                src={image.croppedImageUrl + scalingOptions}
+                alt="import"
+                className="main"
+              ></img>
+              <div className="dimentions">
+                {image.hight}x{image.length}cm{" "}
+              </div>
             </Dialog.Description>
 
             <p></p>
