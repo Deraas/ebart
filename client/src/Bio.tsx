af30dcf9ff30704dba04f747750950134ebf19a0
Deraas <Bendik.Deraas@gmail.com>
2022-12-07 12:23:28 +0100

    Added signature header

--- client/src/Bio.tsx
+++ client/src/Bio.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { useBio } from "./useBio";
+import Signature from "./assets/signature.svg";
 
 function Bio() {
   const { data } = useBio();
@@ -12,18 +13,20 @@ function Bio() {
   return (
     <>
       <div className="bioContainer">
-        <img
-          src={data.map((bio: any) => bio.imageUrl + "?w=1000&fit=scale")}
-          alt="import"
-          className="bioImage"
-        />
-        <div className="bioTextContainer">
-          <h2 className="bioTitle">{data.map((bio: any) => bio.title)}</h2>
-          <div>{data.map((bio: any) => bio.text)}</div>
-          <a href="https://nkl.snl.no/Elisabeth_Balstad" target="blank">
-            {" "}
-            Histore og CV finner du her
-          </a>
+        <img className="signatureHeader" src={Signature}></img>
+        <div>
+          <img
+            src={data.map((bio: any) => bio.imageUrl + "?w=1000&fit=scale")}
+            alt="import"
+            className="bioImage"
+          />
+          <div className="bioTextContainer">
+            <div>{data.map((bio: any) => bio.text)}</div>
+            <a href="https://nkl.snl.no/Elisabeth_Balstad" target="blank">
+              {" "}
+              Histore og CV finner du her
+            </a>
+          </div>
         </div>
       </div>
     </>
